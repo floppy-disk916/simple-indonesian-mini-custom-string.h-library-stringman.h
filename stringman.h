433d2bd6a5b08a7c3a4d264b34b3464135c82a4e
floppy-disk916 <mrifqi29121@gmail.com>
2025-08-27 13:05:59 +0700

    Create stringman.h

---
+++ stringman.h
@@ -0,0 +1,10 @@
+#ifndef STRINGMAN_H
+#define STRINGMAN_H   //just a header guard
+
+#define copystr kopistr
+#define cchar hitungkar
+
+void kopistr(char* dest, const char* src);
+int hitungkar(const char* format);
+
+#endif
